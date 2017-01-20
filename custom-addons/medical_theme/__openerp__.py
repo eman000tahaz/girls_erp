c62a9f3f436e78f37bb35bffb4bca12eb101735d
Eman taha <emeytahaz@gmail.com>
2017-01-20 08:20:48 +0200

    [ADD] medical theme module

---
+++ custom-addons/medical_theme/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Medical Theme',
+    'version': '8.0.1.1.0',
+    'category': 'theme',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'author': 'Eman Taha',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
