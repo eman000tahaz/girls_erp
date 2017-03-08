87d9b5b4c8b94de263947fae121e3f32924cd602
Aya Mohammed Ahmed <ayasharafelden707@gmail.com>
2017-03-08 22:10:44 +0200

    add view and models

--- custom-addons/web_tree_image/static/src/js/web_tree_image.js
+++ custom-addons/web_tree_image/static/src/js/web_tree_image.js
@@ -1,7 +1,9 @@
-/*
+/* 
     OpenERP, Open Source Management Solution
     This module copyright (C) 2014 Therp BV (<http://therp.nl>)
                           (C) 2013 Marcel van der Boom <marcel@hsdev.com>
+    Copyright (C) 2016 Serpent Consulting Services Pvt. Ltd.
+                            (<http://www.serpentcs.com>)
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
@@ -17,10 +19,14 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-openerp.web_tree_image = function(instance) {
+odoo.define('web.tree_image', function (require) {
     "use strict";
-    var QWeb = instance.web.qweb;
-    instance.web.list.Image = instance.web.list.Column.extend({
+    var core = require('web.core');
+    var session = require('web.session');
+    var QWeb = core.qweb;
+    var list_widget_registry = core.list_widget_registry;
+
+    var WebTreeImage = list_widget_registry.get('field.binary').extend({
         format: function (row_data, options) {
             /* Return a valid img tag. For image fields, test if the
              field's value contains just the binary size and retrieve
@@ -28,77 +34,36 @@ openerp.web_tree_image = function(instance) {
             Otherwise, assume a character field containing either a
             stock Odoo icon name without path or extension or a fully
             fledged location or data url */
-            var self = this;
-
-            /*
-            Allow image to be displayed in 3 different ways:
-             - 'inline':    display image directly in tree view (default)
-             - 'icon':      display only an icon, show a full screen preview
-                            of the picture on click
-             - 'thumbnail': display image directly in tree view, show a
-                            full screen preview of the picture on click
-            */
-            self.display = self.display || 'thumbnail';
-
-            if (!row_data[self.id] || !row_data[self.id].value) {
+            if (!row_data[this.id] || !row_data[this.id].value) {
                 return '';
             }
-            var value = row_data[self.id].value;
-            if (self.type === 'binary') {
+            var value = row_data[this.id].value, src;
+            if (this.type === 'binary') {
                 if (value && value.substr(0, 10).indexOf(' ') === -1) {
                     // The media subtype (png) seems to be arbitrary
-                    self.src = "data:image/png;base64," + value;
+                    src = "data:image/png;base64," + value;
                 } else {
                     var imageArgs = {
                         model: options.model,
-                        field: self.id,
+                        field: this.id,
                         id: options.id
                     }
-                    if (self.resize) {
-                        imageArgs.resize = self.resize;
+                    if (this.resize) {
+                        imageArgs.resize = this.resize;
                     }
-                    self.src = instance.session.url('/web/binary/image',
-                                                    imageArgs);
+                    src = session.url('/web/binary/image', imageArgs);
                 }
             } else {
-                if (!/\//.test(row_data[self.id].value)) {
-                    self.src = '/web/static/src/img/icons/' +
-                               row_data[self.id].value + '.png';
+                if (!/\//.test(row_data[this.id].value)) {
+                    src = '/web/static/src/img/icons/' + row_data[this.id].value + '.png';
                 } else {
-                    self.src = row_data[self.id].value;
+                    src = row_data[this.id].value;
                 }
             }
-
-            if (self.display == 'icon' || self.display == 'thumbnail')
-            {
-                // use a unique id for the popup DOM node
-                var popupId = "o_web_tree_image_popup-" + row_data.id.value;
-                // use a unique id for the clickable DOM node
-                var clickableId = "o_web_tree_image_clickable-" +
-                                  row_data.id.value;
-
-                if (!$('#' + popupId).length)
-                {
-                    // add full screen preview to DOM
-                    $("body").append(QWeb.render("ListView.row.image.imageData",
-                                                 {widget: self,
-                                                  popupId: popupId}));
-                }
-
-                // defer execution until after this function has returned and
-                // DOM elements have been rendered
-                window.setTimeout(function() {
-                    // enable full screen preview on click on icon
-                        $("#" + clickableId).click(function() {
-                        $('#' + popupId).modal('show');
-                        return false;
-                    });
-                }, 0);
-            }
-
-            return QWeb.render('ListView.row.image',
-                               {widget: self, clickableId: clickableId});
-        },
+            return QWeb.render('ListView.row.image', {widget: this, src: src});
+        }
     });
-    instance.web.list.columns.add('field.image', 'instance.web.list.Image');
-};
+
+    list_widget_registry
+    .add('field.image', WebTreeImage)
+});
